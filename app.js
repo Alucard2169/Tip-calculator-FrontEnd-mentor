df500da660709567ef23bcccdbf7b5f91deffbf7
Alucard <85690382+Alucard2169@users.noreply.github.com>
2022-03-05 16:56:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,123 +1,98 @@
-// defining main variables
-
-const resetButton = document.querySelector('.reset');
 const billValue = document.querySelector('#amount');
-const headCount = document.querySelector('#people');
 const tipButtons = document.querySelectorAll('.tip-btn');
 const customTip = document.querySelector('#custom');
-const errorMsg = document.querySelector("#error");
-const headError = document.querySelector('#error-count')
+const peopleCount = document.querySelector('#people');
 const tipAmountDisplay = document.querySelector('#tip-total');
 const totalAmountDisplay = document.querySelector('#total');
-
-// reset by default
+const resetBtn = document.querySelector('.reset');
+const errorMessage = document.querySelector('#error');
+const peopleError = document.querySelector('#error-count')
+let tipValue = 0;
+let valid = true;
 reset();
 
-//valid flag
-let valid = true;
 
-// main calculator function
-function calculator(tipAmount) {
-    const bill = +billValue.value;
-    const people = +headCount.value;
-    
-    if (valid == false) {
-        tipAmountDisplay.innerText = '$0.00';
-        totalAmountDisplay.innerText = '$0.00';
-    }
-
-    let tipTotal = ((bill / 100) * tipAmount) / people;
-    let total = bill / people + tipTotal;
-
-
-    tipAmountDisplay.innerText = `$ ${tipTotal.toFixed(2)}`
-    totalAmountDisplay.innerText = `$ ${total.toFixed(2)}`
-
-}
-
-
-
-
-
-resetButton.addEventListener('click', () => {
-    reset()
-})
-
-
-
-
-// button input to catch error
 billValue.addEventListener('input', () => {
     let value = billValue.value;
     totalAmountDisplay.innerText = `$ ${(+value).toFixed(2)}`
     if (billValue.value == '') {
         valid = false;
         billValue.value = null;
-        errorMsg.innerText = 'Numerical digits only!!!'
-        errorMsg.classList.add('enable');
+        errorMessage.innerText = 'Numerical digits only!!!'
+        errorMessage.classList.add('enable');
     }
     else if (billValue.value != '') {
         valid = false
-        errorMsg.classList.remove('enable')
+        errorMessage.classList.remove('enable')
     }
     else {
         valid = true;
     }
-
-    
-})
-
-headCount.addEventListener('input', () => {
-    if (+headCount.value%1 != 0) {
-        valid = false;
-        headError.innerText = "Zombies are not allowed";
-        headError.classList.add('enable');
-
-    }
-    else if(headCount.value > 100 || headCount.value < 1) {
-        valid = false;
-        headError.innerText = 'Seriously!? no more than 100';
-        headError.classList.add('enable');
-    }
-    
-    else{
-        valid = true;
-        headError.classList.remove('enable')
-    }
 })
 
 
-// tip input through button
 tipButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (valid == false) return;
-        calculator((+button.value))
+    button.addEventListener('click', (e) => {
+        peopleCount.value = null;
+        tipValue = +button.value;
     })
 })
-
-
-// tip input through custom
 customTip.addEventListener('input', () => {
-    if (valid == false) return;
-    calculator((+customTip.value))
+    peopleCount.value = null;
+    tipValue = +customTip.value;
 })
 
 
-//reset function
-function reset() {
-    billValue.value = null;
-    headCount.value = null;
-    tipButtons.value = null;
-    customTip.value = null;
-    errorMsg.classList.remove('enable');
-    headError.classList.remove('enable')
-    tipAmountDisplay.innerText = '$0.00';
-    totalAmountDisplay.innerText = '$0.00';
-    headError.setAttribute('aria-invalid',false)
+peopleCount.addEventListener('input', () => {
+    if (+peopleCount.value%1 != 0) {
+        valid = false;
+        peopleError.innerText = "Zombies are not allowed";
+        peopleError.classList.add('enable');
+    }
+    
+    else if (peopleCount.value > 100 || peopleCount.value < 1) {
+        valid = false;
+        peopleError.innerText = 'Seriously!? no more than 100';
+        peopleError.classList.add('enable');
+    }
+   
+    else {
+           valid = true;
+           peopleError.classList.remove('enable')
+           calculator(+peopleCount.value)
+    }
+    
+})
+
+
+
+function calculator(people) {
+    let bill = billValue.value;
+    let tip = tipValue;
+    let peopleValue = people;
+
+    let tipTotal = ((bill / 100) * tip) / peopleValue;
+    let total = bill / peopleValue + tipTotal;
+
+    tipAmountDisplay.innerText = `$ ${tipTotal.toFixed(2)}`;
+    totalAmountDisplay.innerText = `$ ${total.toFixed(2)}`;
 }
 
 
 
+function reset() {
+    tipButtons.forEach((button) => { button.classList.remove('clicked') });
+    billValue.value = null;
+    tipValue = 0;
+    customTip.value = null;
+    peopleCount.value = null;
+    tipAmountDisplay.innerText = "$0.00";
+    totalAmountDisplay.innerText = "$0.00";
+    errorMessage.classList.remove('enable');
+    peopleError.classList.remove('enable');
+}
 
 
-
+resetBtn.addEventListener('click', () => {
+    reset();
+})
